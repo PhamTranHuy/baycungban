081de6388ad09972234e49fae90b851f59644cc1
huy <phamtranhuy998@gmail.com>
2022-04-07 17:14:13 +0700

    create header component, set up tailwind for react

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="fixed">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Header
