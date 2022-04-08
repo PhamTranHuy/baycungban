dc6466982ad0986dfeea3a8117c92192f4e687aa
huy <phamtranhuy998@gmail.com>
2022-04-09 02:40:50 +0700

    update the airline's logo and name style

--- src/components/FlightSchedule/FlightsInfo/Flight.jsx
+++ src/components/FlightSchedule/FlightsInfo/Flight.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
+const FLIGHT = {
+    AirlineCode: "VJ",
+}
 function Flight() {
     return (
-        <div>
-            <div>
-                <div>
-                    <div>logo</div>
-                    <div>Vietnam Airlines</div>
+        <div className="bg-white rounded-xl p-4">
+            <div className="flex text-sm">
+                <div className="flex-center">
+                    <div className="w-[30px] h-[30px] flex-center border rounded-md">
+                        <img className="w-5 h-auto" src={process.env.PUBLIC_URL + '/assets/flight-schedule/VJ.png'} alt="" />
+                    </div>
+                    <div className="ml-3 font-bold tracking-widest">VIETNAM AIRLINES</div>
                 </div>
                 <div>
                     <div>
