2d816f26f6cf105fca0661bd89033a46de61e4e0
huy <phamtranhuy998@gmail.com>
2022-04-08 20:29:12 +0700

    create Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div>
+            <div>Call us: +84 908 02 02 58</div>
+            <div>Email: chucinog@gmail.com</div>
+        </div>
+        <div>
+            <div>Follow us</div>
+            <hr />
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
